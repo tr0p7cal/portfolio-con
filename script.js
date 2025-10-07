b3988064f6d8c10627833e39ad17a46a21fc713d
Con Eichhorn <141146598+tr0p7cal@users.noreply.github.com>
2025-10-07 21:40:00 +0200

    Add files via upload
    hoffentlich letzter commit mit letzten bug fixes für das hamburger dropdown menu und css bug fixes für mobile version.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger-menu');
     const nav = document.getElementById('main-nav');
+    
 
-    // Klick-Event für das Hamburger-Menü
     hamburger.addEventListener('click', function() {
         hamburger.classList.toggle('is-active');
         nav.classList.toggle('is-active');
     });
 
-    // Menü schließen, wenn auf einen Link geklickt wird
+
     const navLinks = document.querySelectorAll('#main-nav a');
+
+
     navLinks.forEach(function(link) {
         link.addEventListener('click', function() {
             if (nav.classList.contains('is-active')) {
+
+                hamburger.classList.remove('is-active');
                 nav.classList.remove('is-active');
             }
         });
