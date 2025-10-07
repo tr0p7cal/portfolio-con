c7cc3eac56ec4e069f4c88bf9a0eaab50a62ea3b
Con Eichhorn <141146598+tr0p7cal@users.noreply.github.com>
2025-10-07 21:49:04 +0200

    Add files via upload
    weiter versuch bug fixes in der mobile version zu beheben. so viele commits, da die bugs für mobile nur durch commits gefunden werden können.

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     const hamburger = document.getElementById('hamburger-menu');
     const nav = document.getElementById('main-nav');
-    
-
-    hamburger.addEventListener('click', function() {
-        hamburger.classList.toggle('is-active');
-        nav.classList.toggle('is-active');
-    });
-
-
     const navLinks = document.querySelectorAll('#main-nav a');
 
 
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function() {
-            if (nav.classList.contains('is-active')) {
+    function closeMenu() {
+        hamburger.classList.remove('is-active');
+        nav.classList.remove('is-active');
+    }
 
-                hamburger.classList.remove('is-active');
-                nav.classList.remove('is-active');
-            }
-        });
+    function toggleMenu() {
+        hamburger.classList.toggle('is-active');
+        nav.classList.toggle('is-active');
+    }
+
+    hamburger.addEventListener('click', toggleMenu);
+
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', closeMenu);
     });
 });
